225d0e60b61643828bf59dee56bd8aa1d3b68049
star <906187576@qq.com>
2020-12-07 12:07:17 +0800

    LogUploader增加输出代码

--- dw-mocker/src/main/java/utils/LogUploader.java
+++ dw-mocker/src/main/java/utils/LogUploader.java
@@ -37,6 +37,7 @@ public class LogUploader {
             out.close();
             int code = conn.getResponseCode();
             System.out.println(code);
+            System.out.println("mygithub");
         } catch (Exception e) {
             e.printStackTrace();
         }
